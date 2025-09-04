497e33b6a7136a98b3fa15837328d15db59729b7
girlanasouza <girlanasouza123@gmail.com>
2025-09-04 16:47:19 -0300

    correção de nomes

--- scoreboard.py
+++ scoreboard.py
@@ -10,6 +10,7 @@ OPCODES = {
     'fdiv': 5
 }
 
+# Define register prefix constants
 REG_PREFIXES = {
     'x': 'int',
     'f': 'float'
@@ -18,45 +19,28 @@ REG_PREFIXES = {
 def has_pending_instructions(inst_status):
     return any(inst['write_result'] is None for inst in inst_status)
 
-def formatar_tabela_scoreboard(instruction_status):
-    df = pd.DataFrame(instruction_status)
-    tabela_final = df[['inst', 'issue', 'read_operands', 'execution_complete', 'write_result']]
-
-    tabela_final = tabela_final.rename(columns={
-        'inst': 'Instruction/Cicle',
-        'issue': 'Issue',
-        'read_operands': 'Read',
-        'execution_complete': 'Execute', 
-        'write_result': 'Write'
-    })
-
-    tabela_final = tabela_final.fillna("-")
-
-    return tabela_final
-
-# ----------------------------------------------------------------------- #
-
-# INIALIZAÇÃO DOS REGISTRADORES
 def init_register_status():
     register_status = {}
     for i in range(32):
-        register_status[f"x{i}"] = {"x": i, "x_type": "int", "writer": None} # x0...x31
+        register_status[f"x{i}"] = {"x": i, "x_type": "int", "writer": None}
     for i in range(32):
-        register_status[f"f{i}"] = {"f": i, "f_type": "float", "writer": None} # f1...f31
+        register_status[f"f{i}"] = {"f": i, "f_type": "float", "writer": None}
     return register_status
 
-
-# PARSER E INICIALIZAÇÃO DAS INSTRUÇÕES 
 def init_instruction_status(program_name):
     instruction_status =[]
     with open(program_name, "r") as file:
         for i, line in enumerate(file):
             op = OPCODES[line.split()[0]]
-            if (op == 0 or op==1):
+            if (op == 0):
                 rd = line.split()[1].replace(",", "")
                 rs1 = line.split()[2].split("(")[1].replace(")", "")
                 rs2 = None
-                imm = line.split()[2].split("(")[0]
+                fu_type = 0
+            elif (op==1):
+                rd = None
+                rs1 = line.split()[1].replace(",", "")
+                rs2 = None
                 fu_type = 0
             else:
                 if op == 2 or op == 3: fu_type = 2
@@ -65,8 +49,8 @@ def init_instruction_status(program_name):
                 rd = line.split()[1].replace(",", "")
                 rs1 = line.split()[2].replace(",", "")
                 rs2 = line.split()[3].replace(",", "")
-                imm = None          
-            instruction_status.append({ 
+                imm = None
+            instruction_status.append({
                 "id": i,
                 "inst": line.replace("\n", ""),
                 "opcode": op,
@@ -83,7 +67,6 @@ def init_instruction_status(program_name):
             })
     return  instruction_status
 
-# PARSER E INICIALIZAÇÃO DAS UNIDADES FUNCIONAIS 
 def parser_fus_configs(config_name):
     fus_configs = {}
     with open(config_name, 'r') as f:
@@ -107,46 +90,41 @@ def init_fus_status(programa_config):
             op = 3
         for c in range(int(config['qtd'])):
             fus_status[f'{uf_type}{c+1}'] = {
-                'Opcode': op, 
-                'Busy': False, 
-                'Op': None, 
+                'Opcode': op,
+                'Busy': False,
+                'just_freed': False,
+                'Op': None,
                 'Fi': None,
-                'Fj': None, 
-                'Fk': None, 
-                'Qj': None, 
+                'Fj': None,
+                'Fk': None,
+                'Qj': None,
                 'Qk': None,
-                'Rj': False, 
-                'Rk': False, 
-                'Cycles_left': int(config['cycles']), 
+                'Rj': False,
+                'Rk': False,
+                'Cycles_left': int(config['cycles']),
                 'Cycles': int(config['cycles'])
             }
     return fus_status
 
-## ESTÁGIO: ISSUE
-def issue(instr, fus_status, register_status, fu_name_i):
-    opcode_i = instr['opcode']
+
+def issue(instr, fus_status, register_status, fus_liberadas_no_ciclo):
     id_i = instr['id']
     rs1_i = instr['rs1']
     rs2_i = instr['rs2']
     rd_i = instr['rd']
     fu_type = instr['fu_type']
-
     for fu_name, fu in fus_status.items():
-
-        if fu_name_i == fu_name:
-            return False
-        # 1. opcode não bate
         if fu['Opcode'] != fu_type:
             continue
 
-        # 2. FU já ocupada
         if fu['Busy']:
             continue
 
-        # 3. registrador destino já tem writer
         if rd_i and register_status[rd_i]['writer'] is not None:
             return False
-        
+
+        if fu_name in fus_liberadas_no_ciclo:
+            return False
 
         fu['Busy'] = True
         fu['Op'] = id_i
@@ -157,24 +135,24 @@ def issue(instr, fus_status, register_status, fu_name_i):
         fu['Qk'] = register_status[rs2_i]['writer'] if rs2_i is not None else None
         fu['Rj'] = fu['Qj'] is None
         fu['Rk'] = fu['Qk'] is None
+
         if rd_i is not None:
-            register_status[rd_i]['writer'] = fu_name  
+            register_status[rd_i]['writer'] = fu_name
         instr['fu_name'] = fu_name
+
         return True
+
     return False
 
-
-## ESTÁGIO: READ OPERANDS
-def read_operands(fu, inst, register_status, instruction_status, cycle):
+def read_operands(fu, inst, register_status, cycle):
     if not fu['Rj']:
         writer_j = register_status.get(fu['Fj'], {}).get('writer')
-        if writer_j is None:  # ninguém está escrevendo em Rj
+        if writer_j is None: 
             fu['Rj'] = True
             fu['Qj'] = None
-        # se alguem estver escrevendo -- Rj não fica true e não consegue avançar na leitura
 
     if not fu['Rk']:
-        if not fu['Fk']:  # não tem Fk
+        if not fu['Fk']:  
             fu['Rk'] = True
         else:
             writer_k = register_status.get(fu['Fk'], {}).get('writer')
@@ -187,7 +165,6 @@ def read_operands(fu, inst, register_status, instruction_status, cycle):
         fu['Rj'] = False
         fu['Rk'] = False
 
-## ESTÁGIO: COMPLETE EXECUTION
 def execute(fu, cycle, inst):
     if fu['Cycles_left'] > 0:
         fu['Cycles_left'] -= 1
@@ -195,60 +172,57 @@ def execute(fu, cycle, inst):
         inst['execution_complete'] = cycle
         fu['Cycles_left'] = fu['Cycles']
 
-## ESTÁGIO: WRITE RESULTS
 def write_results(fus_status, inst, cycle, register_status):
     fu = fus_status[inst['fu_name']]
-    fi_register = fu['Fi'] # O registrador de destino desta instrução
+    fi_register = fu['Fi']
 
-    # --- VERIFICAÇÃO DE HAZARD WAR ---
-    # Itera sobre todas as unidades funcionais para ver se alguma precisa ler nosso registrador de destino.
-    for other_fu_name, other_fu in fus_status.items():
-        # Ignora a verificação contra si mesma
-        if other_fu_name == inst['fu_name']:
-            continue
+    if fi_register:
+        for other_fu_name, other_fu in fus_status.items():
+            if other_fu_name == inst['fu_name']:
+                continue
 
-        # Condição de Hazard: Outra FU ainda não leu (Rj=False) e seu operando fonte (Fj)
-        # é o nosso registrador de destino (Fi).
-        if other_fu['Rj'] and other_fu['Fj'] == fi_register:
-            return None, None # Stall! Não faz nada neste ciclo.
+            if other_fu['Rj'] and other_fu['Fj'] == fi_register:
+                return None, None 
 
-        # Repete a verificação para o segundo operando (Fk)
-        if other_fu['Rk'] and other_fu['Fk'] == fi_register:
-            return None, None # Stall!True
+            if other_fu['Rk'] and other_fu['Fk'] == fi_register:
+                return None, None 
 
-    # --- Se passou na verificação (sem hazards), procede com o Write Back ---
     inst['write_result'] = cycle
-
-    if fi_register and register_status[fi_register]['writer'] == inst['fu_name']:
-        register_status[fi_register]['writer'] = None
-
     fu.update({
         'Busy': False, 'Op': None, 'Fi': None, 'Fj': None, 'Fk': None,
         'Qj': None, 'Qk': None, 'Rj': False, 'Rk': False
     })
+
+    if fi_register and register_status[fi_register]['writer'] == inst['fu_name']:
+        register_status[fi_register]['writer'] = None
     return fi_register, inst['fu_name']
 
-## SCOREBOARD
 def scoreboard(prog_file, fus_file):
     register_status = init_register_status()
-    instruction_status = init_instruction_status(instr_files)
-    fus_status = init_fus_status(fus_files)
+    instruction_status = init_instruction_status(prog_file)
+    fus_status = init_fus_status(fus_file)
+
     pc = 0
     cycle = 1
 
     while pc < len(instruction_status) or has_pending_instructions(instruction_status):
-        print(f"\n--- Cycle {cycle} ---")
-        print(f"PC = {pc}")
+
         liberar_write = False
         fu_name_i = ""
-        # WRITE RESULTS
+        registradores_escritos_no_ciclo = set()
+        fus_liberadas_no_ciclo = set()
         for inst in instruction_status:
             if inst['execution_complete'] is not None and inst['execution_complete'] < cycle and inst['write_result'] is None:
                 resultado = write_results(fus_status, inst, cycle, register_status)
                 if resultado != (None, None):
                     fi_escrita, fu_name_i = resultado
+                    if fi_escrita:
+                        registradores_escritos_no_ciclo.add(fi_escrita)
+
+                    if fu_name_i:
+                        fus_liberadas_no_ciclo.add(fu_name_i)
                     liberar_write = True
-                
+
         for inst in instruction_status:
             if inst['read_operands'] is not None and inst['read_operands'] < cycle and inst['execution_complete'] is None:
                 fu = fus_status[inst['fu_name']]
@@ -257,45 +231,48 @@ def scoreboard(prog_file, fus_file):
         # 1. Read operands
         for inst in instruction_status:
             if inst['issue'] is not None and inst['read_operands'] is None:
-                if liberar_write:
-                    print(f"[DEBUG] Ciclo {cycle}: leitura bloqueada porque houve write")
-                    continue  # pula leitura neste ciclo
-                
-                print(f"\n[DEBUG] Ciclo {cycle} - Checando instrução '{inst['inst']}' (FU={inst['fu_name']})")
-
                 fu = fus_status[inst['fu_name']]
-                read_operands(fu, inst, register_status, instruction_status, cycle)
+                if fu['Fj'] in registradores_escritos_no_ciclo or fu['Fk'] in registradores_escritos_no_ciclo:
+                    continue  # Pula a leitura APENAS para esta instrução
+                read_operands(fu, inst, register_status, cycle)
 
-        # 2. Issue
-        if pc < len(instruction_status):  
+        if pc < len(instruction_status):
             next_instruction = instruction_status[pc]
             if liberar_write and next_instruction['rd'] == fi_escrita:
                 pass
             else:
-                print(f"entrei aqui para a instrução {next_instruction}")
-                if issue(next_instruction, fus_status, register_status, fu_name_i):
-                    next_instruction['issue'] = cycle
-                    print(f"--> Instr {next_instruction['id']} emitida")
-                    pc += 1
+                fu_type = next_instruction['fu_type']
+                if any(fus_status[fu]['Opcode'] == fu_type and fu in fus_liberadas_no_ciclo for fu in fus_status):
+                    pass
                 else:
-                    print(f"--> Instr {next_instruction['id']} NÃO pôde ser emitida")
-                    # print(issue(next_instruction, fus_status, register_status))
+                    if issue(next_instruction, fus_status, register_status, fus_liberadas_no_ciclo):
+                        next_instruction['issue'] = cycle
+                        pc += 1
+
 
         cycle += 1
-
-
     return instruction_status
 
+def formatar_tabela_scoreboard(instruction_status):
+    df = pd.DataFrame(instruction_status)
+    tabela_final = df[['inst', 'issue', 'read_operands', 'execution_complete', 'write_result']]
 
-
-import os
+    tabela_final = tabela_final.rename(columns={
+        'inst': 'Instruction/Cicle',
+        'issue': 'Issue',
+        'read_operands': 'Read',
+        'execution_complete': 'Execute',
+        'write_result': 'Write'
+    })
+    tabela_final = tabela_final.fillna("-")
+    return tabela_final
 
 if __name__ == "__main__":
     # listas de programas e configs
-    instr_files = [f"t1/testes/instrucoes_{i}.txt" for i in range(1, 9)]
-    fus_files   = [f"t1/testes/unidades_{i}.txt"   for i in range(1, 9)]
+    instr_files = [f"./tests/in_puts_insts/t{i}.S" for i in range(1, 14)] # mudar aqui para testar
+    fus_files   = [f"./tests/in_puts_configs/fu_config{i}.in"   for i in range(1, 14)] # mudar aqui para testar
 
-    output_file = "resultados.txt"
+    output_file = "./tests/out_puts/resultados.out"
 
     with open(output_file, "w") as out:
         for i in range(len(instr_files)):
@@ -309,18 +286,3 @@ if __name__ == "__main__":
 
             out.write(tabela_de_resultados.to_string(index=False))
             out.write("\n\n")
-
-
-# if __name__ == "__main__":
-#     instr_files = [f"instrucoes_{i}.txt" for i in range(1, 9)]
-#     fus_files   = [f"unidades_{i}.txt"   for i in range(1, 9)]
-
-#     output_file = "resultados.txt"
-
-    # prog_file = "tests/ex4.s" # nome do arquivo do programa assembly
-    # fus_file = "tests/uf_config4.in" # nome do programa com as configuraçoes das UFs
-    
-    # instruction_status = scoreboard(prog_file, fus_file)
-
-    # tabela_de_resultados = formatar_tabela_scoreboard(instruction_status)
-    # print(tabela_de_resultados.to_string(index=False))
