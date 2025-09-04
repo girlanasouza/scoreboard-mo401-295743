0e552aa305eec06358d7f881584892f568821611
girlanasouza <girlanasouza123@gmail.com>
2025-09-04 16:46:20 -0300

    adicionando a versão final do trabalho

--- parser_config.py
+++
@@ -1,8 +0,0 @@
-def parse_config(arquivo):
-    fus_configs = {}
-    with open("tests/uf_config2.in", 'r') as f:
-        linhas = f.readlines()
-        for linha in linhas:
-            partes = linha.strip().split()
-            fus_configs[partes[0]] = {'qtd': partes[1], 'cycles': partes[2]}
-    return fus_configs
